749c5210cc54152035a6d70944b2d1edc6f017b6
Eric Webber <ewebber283@gmail.com>
2021-05-12 18:31:05 -0500

    added first inquirer prompt

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+    .prompt([
+        {
+            type: 'imput',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
 //holds command line arguments
-
-const profileDataArgs = process.argv.slice(2);
+//const profileDataArgs = process.argv.slice(2);
 // extract arguments and store in variable
-const [name, github] = profileDataArgs;
-
+//const [name, github] = profileDataArgs;
+//const pageHTML = generatePage(name, github);
 // generates the page
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new err;
+//fs.writeFile('./index.html', generatePage(name, github), err => {
+  //  if (err) throw new err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-})
+    //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
 
